36fc9b5d2e6ab268882db8d689ffdd2dc9d51b80
AardvarkSoup <tomtervoort@gmail.com>
2013-04-02 18:04:52 +0300

    ToSBV compiles now

--- ToSBV.hs
+++ ToSBV.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE Haskell2010, DeriveDataTypeable #-}
 
 module ToSBV where
 
@@ -9,8 +9,10 @@ import qualified CalculusTypes as C
 import Data.Array (Array, (!))
 import qualified Data.Array as A
 import Data.List
-import Control.Exception
 import Data.Maybe
+import Control.Exception
+import Control.Monad
+
 
 -- syb
 import Data.Generics
@@ -32,29 +34,31 @@ instance Show IllegalVariableException where
 
 instance Exception IllegalVariableException
 
+
 -- | Transforms the intermediate representation of a (weakest) precondition into a Predicate as 
 --   understood by SBV.
 preConditionToPredicate :: Condition -> Predicate
 preConditionToPredicate cond = forAll ["a"] $ preConditionToParamPredicate cond
 
 -- | Transform a condition into a predicate over a parameter array.
-preConditionToParamPredicate :: Condition -> ParamArray -> SBool
+preConditionToParamPredicate :: Condition -> ParamArray -> Predicate
 preConditionToParamPredicate cond params = c2p [] cond
  where c2p scoped cond = let e2i = expressionToSInteger params scoped in
         case cond of
-         C.Forall n c -> forAll  [n] $ \v -> c2p (insert (n, v) scoped) c
-         C.Exists n c -> forSome [n] $ \v -> c2p (insert (n, v) scoped) c
-         C.True       -> literal True
-         C.False      -> literal False
-         C.Not c      -> bnot $ c2p scoped c
-         C.And a b    -> c2p scoped a &&& c2p scoped b
-         C.Or  a b    -> c2p scoped a ||| c2p scoped b
-         C.EQ  a b    -> e2i a .== e2i b
-         C.NEQ a b    -> e2i a ./= e2i b
-         C.GT  a b    -> e2i a .>  e2i b
-         C.GTE a b    -> e2i a .>= e2i b
-         C.LT  a b    -> e2i a .<  e2i b
-         C.LTE a b    -> e2i a .<= e2i b
+         C.Forall n c -> forAll  [n] $ \v -> c2p ((n,v) : scoped) c
+         C.Exists n c -> forSome [n] $ \v -> c2p ((n,v) : scoped) c
+         C.Not c      -> liftM bnot $ c2p scoped c
+         C.And a b    -> liftM2 (&&&) (c2p scoped a) (c2p scoped b)
+         C.Or  a b    -> liftM2 (|||) (c2p scoped a) (c2p scoped b)
+         _ -> forAll_ $ case cond of
+                         C.True       -> literal True
+                         C.False      -> literal False
+                         C.EQ  a b    -> e2i a .== e2i b
+                         C.NEQ a b    -> e2i a ./= e2i b
+                         C.GT  a b    -> e2i a .>  e2i b
+                         C.GTE a b    -> e2i a .>= e2i b
+                         C.LT  a b    -> e2i a .<  e2i b
+                         C.LTE a b    -> e2i a .<= e2i b
        
 -- | Converts an expression to a symbolic integer based on the parameters and the current 
 --   environment of scoped variables.
