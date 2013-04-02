d785c619539252707eeaa04c6c8cdde2da8fddbd
Tom Tervoort <tomtervoort@gmail.com>
2013-04-02 17:48:20 +0200

    Added Read to SymbolicPrelude.

--- SymbolicPrelude.hs
+++ SymbolicPrelude.hs
@@ -3,6 +3,7 @@ module SymbolicPrelude (module Prelude) where
 import Prelude ( Eq(..)
                , Num(..)
                , Show
+               , Read
                , String
                , Int
                , Integer
