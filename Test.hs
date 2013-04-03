b093c34394e8adea0e0d157426d058ba86916709
Tom Tervoort <tomtervoort@gmail.com>
2013-04-03 18:05:21 +0200

    max4 toegevoegd als test.

--- Test.hs
+++ Test.hs
@@ -7,10 +7,12 @@ import qualified CalculusTypes as C
 import ProgramTypes
 import WP
 
+arg = Var . Argument
+
 test1 = (True, (Var Return `C.EQ` 130),           [START 0, PUSHLITERAL 100, PUSHLITERAL 30, ADD, RETURN])
 test2 = (True, (Var Return `C.EQ` 130),           [START 0, PUSHLITERAL 0, IFTRUE [PUSHLITERAL 100] [PUSHLITERAL 200], PUSHLITERAL 30, ADD, RETURN])
 test3 = (True, (Var Return `C.EQ` 130),           [START 0, PUSHLITERAL 1, IFTRUE [PUSHLITERAL 100] [PUSHLITERAL 200], PUSHLITERAL 30, ADD, RETURN])
-test4 = (True, (Var Return `C.EQ` Var (Param 1)), [START 1, PUSHLITERAL 1, RETURN])
+test4 = (True, (Var Return `C.EQ` arg 1), [START 1, PUSHLITERAL 1, RETURN])
 test5 = (True, (Var Return `C.EQ` 10),            [START 2, LOADPARAM 1, LOADPARAM 2, LTE, IFTRUE [PUSHLITERAL 10] [PUSHLITERAL 20], RETURN])
 test6 = (True, (C.EQ (Var Return) (Literal 10)),  [START 1, LOADPARAM 0, IFTRUE [PUSHLITERAL 20] [PUSHLITERAL 10], LOADPARAM 0, IFTRUE [PUSHLITERAL 10] [], RETURN])
 test7 = (True, (C.EQ (Var Return) (Literal 10)),  [ START 1
@@ -35,6 +37,42 @@ test8 = (Var (Argument 0) `C.EQ` (Literal 10),
                                                      []
                                                   , RETURN ])
 test9 = (True, (C.EQ (Var Return) (Literal 2)), [START 0, SETLOCAL 0 0, PUSHLITERAL 1, WHILETRUE [PUSHLITERAL 2, LOADLOCAL 0, PUSHLITERAL 1, ADD, STORELOCAL 0, LOADLOCAL 0, GT], LOADLOCAL 0, RETURN])
+
+example1 = (True, 
+            (C.GTE (Var Return) (arg 0)) `And` (C.GTE (Var Return) (arg 1)) `And` (C.GTE (Var Return) (arg 2)) `And` (C.GTE (Var Return) (arg 3)),
+            
+            [	START 4,
+                LOADPARAM 0,
+                LOADPARAM 1,
+                LOADPARAM 2,
+                LOADPARAM 3,
+                
+                STORELOCAL 0,
+                
+                STORELOCAL 1,
+                LOADLOCAL 0,
+                LOADLOCAL 1,
+                LT,
+                IFTRUE [LOADLOCAL 1, STORELOCAL 0] [],
+                
+                STORELOCAL 1,
+                LOADLOCAL 0,
+                LOADLOCAL 1,
+                LT,
+                IFTRUE [LOADLOCAL 1, STORELOCAL 0] [],
+                
+                STORELOCAL 1,
+                LOADLOCAL 0,
+                LOADLOCAL 1,
+                LT,
+                IFTRUE [LOADLOCAL 1, STORELOCAL 0] [],
+                
+                LOADLOCAL 0,
+                
+                RETURN
+            ]
+           )
+
 
 test (pre, post, prog) = mapM_ (\(x, y) -> putStrLn $ x ++ y) $ zip (map (width 25) $ map ppi prog ++ ["---END---"]) (map ppc $ wps prog post)
   where width :: Int -> String -> String
